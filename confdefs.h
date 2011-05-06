c35b43c577003e6d1a565e9a1dd8bc817472aac3
Matthias Klose <doko@ubuntu.com>
2011-05-06 19:38:21 +0200

    2.3.5-2ubuntu1 (patches unapplied)
    Imported using git-ubuntu import.

---
+++ confdefs.h
@@ -0,0 +1,9 @@
+/* confdefs.h */
+#define PACKAGE_NAME "lasso"
+#define PACKAGE_TARNAME "lasso"
+#define PACKAGE_VERSION "2.3.5"
+#define PACKAGE_STRING "lasso 2.3.5"
+#define PACKAGE_BUGREPORT "lasso-devel@lists.labs.libre-entreprise.org"
+#define PACKAGE_URL ""
+#define PACKAGE "lasso"
+#define VERSION "2.3.5"
