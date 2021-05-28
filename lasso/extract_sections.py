f0bbdaa4792bb102dbede57e3d20326e25a53b42
Steve Beattie <sbeattie@ubuntu.com>
2021-05-28 14:49:51 -0700

    build-without-python2.diff
    No DEP3 Subject or Description header found
    
    Gbp-Pq: build-without-python2.diff.

--- lasso/extract_sections.py
+++ lasso/extract_sections.py
@@ -1,10 +1,11 @@
 #! /usr/bin/env python
 
-import glob
+import io
 import re
 import sys
 import os
 import os.path
+from six import print_
 
 enable_wsf = False
 
@@ -23,8 +24,8 @@ for root, dirs, files in os.walk(srcdir):
             prefixes.append(os.path.splitext(file)[0])
     for prefix in prefixes:
         try:
-            header = open(os.path.join(root, prefix + '.h')).read()
-            implementation = open(os.path.join(root, prefix + '.c')).read()
+            header = io.open(os.path.join(root, prefix + '.h'), encoding='utf-8').read()
+            implementation = io.open(os.path.join(root, prefix + '.c'), encoding='utf-8').read()
             exported_functions = re.findall('LASSO_EXPORT.*(lasso_\w*)', header)
             normal_functions = sorted ([ x for x in exported_functions if not x.endswith('get_type') ])
             get_type = [ x for x in exported_functions if x.endswith('get_type') ][0]
@@ -45,17 +46,17 @@ for root, dirs, files in os.walk(srcdir):
                     return x
             if type and macro_type:
                 standard_decl = [ convert(x) for x in [ 'LASSO_%s', 'LASSO_IS_%s', 'LASSO_TYPE_%s', get_type, 'LASSO_%s_CLASS', 'LASSO_IS_%s_CLASS', 'LASSO_%s_GET_CLASS' ] ]
-                print
-                print '<SECTION>'
-                print '<FILE>%s</FILE>' % file_name
-                print '<TITLE>%s</TITLE>' % type
-                print type
+                print_('')
+                print_('<SECTION>')
+                print_('<FILE>%s</FILE>' % file_name)
+                print_('<TITLE>%s</TITLE>' % type)
+                print_(type)
                 for x in types + normal_functions:
-                    print x
-                print '<SUBSECTION Standard>'
+                    print_(x)
+                print_('<SUBSECTION Standard>')
                 for x in standard_decl:
-                    print x
-                print '</SECTION>'
+                    print_(x)
+                print_('</SECTION>')
         except:
             continue
 
